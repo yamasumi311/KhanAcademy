dfd96164a700cb5b3f1a961887ef8c4d306835a9
yamasumi311 <chim.0.don@gmail.com>
2023-01-30 11:06:44 +1100

    sync with html

--- fish.js
+++ fish.js
@@ -1,55 +1,68 @@
-background(89, 216, 255);
+function fish(processingJS) {
+    with (processingJS) {
+        size(400, 400);
 
-var drawFish = function (centerX, centerY, bodyLength, bodyHeight, bodyColor) {
+        background(89, 216, 255);
 
-    noStroke();
-    fill(bodyColor);
-    // body
-    ellipse(centerX, centerY, bodyLength, bodyHeight);
-    // tail
-    var tailWidth = bodyLength / 4;
-    var tailHeight = bodyHeight / 2;
-    triangle(centerX - bodyLength / 2, centerY,
-        centerX - bodyLength / 2 - tailWidth, centerY - tailHeight,
-        centerX - bodyLength / 2 - tailWidth, centerY + tailHeight);
-    // eye
-    fill(33, 33, 33);
-    ellipse(centerX + bodyLength / 4, centerY, bodyHeight / 5, bodyHeight / 5);
-};
+        var drawFish = function (centerX, centerY, bodyLength, bodyHeight, bodyColor) {
 
-for (var i = 0; i < 10; i++) {
-    var bodyHeight;
-    var bodyLength;
-    var centerX = random(0, 400);
-    var centerY = random(0, 400);
-    var a = random(10, 100);
-    var b = random(10, 100);
-    if (a < b) {
-        bodyHeight = a;
-        bodyLength = b;
-    } else {
-        bodyHeight = b;
-        bodyLength = a;
+            noStroke();
+            fill(bodyColor);
+            // body
+            ellipse(centerX, centerY, bodyLength, bodyHeight);
+            // tail
+            var tailWidth = bodyLength / 4;
+            var tailHeight = bodyHeight / 2;
+            triangle(centerX - bodyLength / 2, centerY,
+                centerX - bodyLength / 2 - tailWidth, centerY - tailHeight,
+                centerX - bodyLength / 2 - tailWidth, centerY + tailHeight);
+            // eye
+            fill(33, 33, 33);
+            ellipse(centerX + bodyLength / 4, centerY, bodyHeight / 5, bodyHeight / 5);
+        };
+
+        for (var i = 0; i < 10; i++) {
+            var bodyHeight;
+            var bodyLength;
+            var centerX = random(0, 400);
+            var centerY = random(0, 400);
+            var a = random(10, 100);
+            var b = random(10, 100);
+            if (a < b) {
+                bodyHeight = a;
+                bodyLength = b;
+            } else {
+                bodyHeight = b;
+                bodyLength = a;
+            }
+            var myColor = color(random(0, 255), random(0, 255), random(0, 255));
+            drawFish(centerX, centerY, bodyLength, bodyHeight, myColor);
+        }
+
+
+        var bubbleSizes = [10, 15, 20, 25];
+
+
+        // makes bubbles when mouse is pressed
+        var lastBubbleSize = 0;
+        function getBubbleSize() {
+            return bubbleSizes[lastBubbleSize++ % bubbleSizes.length];
+        }
+
+
+        mousePressed = function () {
+            fill(255, 255, 255);
+            var bubbleSize = getBubbleSize();
+            ellipse(mouseX, mouseY, bubbleSize, bubbleSize);
+        };
     }
-    var myColor = color(random(0, 255), random(0, 255), random(0, 255));
-    drawFish(centerX, centerY, bodyLength, bodyHeight, myColor);
 }
 
-
-var bubbleSizes = [10, 15, 20, 25];
-
-
-// makes bubbles when mouse is pressed
-var lastBubbleSize = 0;
-function getBubbleSize() {
-    return bubbleSizes[lastBubbleSize++ % bubbleSizes.length];
-}
+var canvas = document.getElementById("mycanvas");
+new Processing(canvas, fish);
 
 
-mousePressed = function () {
-    fill(255, 255, 255);
-    var bubbleSize = getBubbleSize();
-    ellipse(mouseX, mouseY, bubbleSize, bubbleSize);
-};
+
+
 
 
