538b3f77ded4602978120e34ac37d378de28475c
yamasumi311 <chim.0.don@gmail.com>
2023-02-05 12:51:08 +1100

    rain

---
+++ rain/rain.js
@@ -0,0 +1,35 @@
+var xPositions = [];
+var yPositions = [];
+var dropColors = [];
+var speeds = [];
+
+for (var i = 0; i < 100; i++) {
+    xPositions.push(random(1, 399));
+    yPositions.push(random(1, 399));
+    var dropColor = color(random(0, 255), random(0, 255), random(0, 255));
+    dropColors.push(dropColor);
+    speeds.push(random(1, 5));
+}
+
+draw = function () {
+    background(204, 247, 255);
+
+    noStroke();
+
+    for (var i = 0; i < xPositions.length; i++) {
+        fill(dropColors[i]);
+        ellipse(xPositions[i], yPositions[i], 10, 10);
+        /* if condition version
+            if (yPositions[i] > 400) {
+                yPositions[i] = 0;    
+            } 
+            else {
+                yPositions[i] += speeds[i];
+            }
+        */
+        // module(remainder%) version
+        yPositions[i] = speeds[i] + yPositions[i] % 400;
+    }
+
+};
+
