bbd0d3e9d7cf60cd2120ce3d910ede935d4d9041
yamasumi311 <chim.0.don@gmail.com>
2023-02-06 10:24:14 +1100

    fish bubble comment on html

--- rain/rain.js
+++ rain/rain.js
@@ -1,35 +1,44 @@
-var xPositions = [];
-var yPositions = [];
-var dropColors = [];
-var speeds = [];
+function house(processingJS) {
+    with (processingJS) {
+        size(400, 400);
 
-for (var i = 0; i < 100; i++) {
-    xPositions.push(random(1, 399));
-    yPositions.push(random(1, 399));
-    var dropColor = color(random(0, 255), random(0, 255), random(0, 255));
-    dropColors.push(dropColor);
-    speeds.push(random(1, 5));
+        var xPositions = [];
+        var yPositions = [];
+        var dropColors = [];
+        var speeds = [];
+
+        for (var i = 0; i < 100; i++) {
+            xPositions.push(random(1, 399));
+            yPositions.push(random(1, 399));
+            var dropColor = color(random(0, 255), random(0, 255), random(0, 255));
+            dropColors.push(dropColor);
+            speeds.push(random(1, 5));
+        }
+
+        draw = function () {
+            background(204, 247, 255);
+
+            noStroke();
+
+            for (var i = 0; i < xPositions.length; i++) {
+                fill(dropColors[i]);
+                ellipse(xPositions[i], yPositions[i], 10, 10);
+                /* if condition version
+                    if (yPositions[i] > 400) {
+                        yPositions[i] = 0;    
+                    } 
+                    else {
+                        yPositions[i] += speeds[i];
+                    }
+                */
+                // module(remainder%) version
+                yPositions[i] = speeds[i] + yPositions[i] % 400;
+            }
+
+        };
+    }
 }
 
-draw = function () {
-    background(204, 247, 255);
-
-    noStroke();
-
-    for (var i = 0; i < xPositions.length; i++) {
-        fill(dropColors[i]);
-        ellipse(xPositions[i], yPositions[i], 10, 10);
-        /* if condition version
-            if (yPositions[i] > 400) {
-                yPositions[i] = 0;    
-            } 
-            else {
-                yPositions[i] += speeds[i];
-            }
-        */
-        // module(remainder%) version
-        yPositions[i] = speeds[i] + yPositions[i] % 400;
-    }
-
-};
+var canvas = document.getElementById("mycanvas");
+new Processing(canvas, house);
 
